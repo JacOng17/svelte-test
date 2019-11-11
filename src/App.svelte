cc1f73d692451c368774803ef786bbef326375de
Jacob <jacob.sien.ong@gmail.com>
2019-11-11 14:00:32 +1300

    New files.

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let name;
+	import Counter from './Counter.svelte'
+	import AddNumber from './AddNumber.svelte'
+	import Info from './Info.svelte'
+	import LoginStatus from './LoginStatus.svelte'
+
+		const pkg = {
+			name: 'svelte',
+			version: 3,
+			speed: 'blazing',
+			website: 'https://svelte.dev'
+		};
+</script>
+
+<h1 class="welcome-name">Welcome {name}!</h1>
+<Counter />
+<AddNumber />
+<Info {...pkg}/>
+<LoginStatus />
